39a0c981102296e8c1f5fca9f3ccb084ed179d10
lukaKry <kryszak.lukasz@gmail.com>
2021-01-20 11:35:30 +0100

    Complete Version; BestScores added

---
+++ Minesweeper_WindowsFormsApp/Form2.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace Minesweeper_WindowsFormsApp
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1_easy = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1_normal = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox2_hard = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1_easy
+            // 
+            this.listBox1_easy.FormattingEnabled = true;
+            this.listBox1_easy.ItemHeight = 16;
+            this.listBox1_easy.Location = new System.Drawing.Point(18, 79);
+            this.listBox1_easy.Name = "listBox1_easy";
+            this.listBox1_easy.Size = new System.Drawing.Size(234, 244);
+            this.listBox1_easy.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(361, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Best Scores";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(85, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Level: easy";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(352, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Level: normal";
+            // 
+            // listBox1_normal
+            // 
+            this.listBox1_normal.FormattingEnabled = true;
+            this.listBox1_normal.ItemHeight = 16;
+            this.listBox1_normal.Location = new System.Drawing.Point(286, 79);
+            this.listBox1_normal.Name = "listBox1_normal";
+            this.listBox1_normal.Size = new System.Drawing.Size(234, 244);
+            this.listBox1_normal.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(621, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Level: hard";
+            // 
+            // listBox2_hard
+            // 
+            this.listBox2_hard.FormattingEnabled = true;
+            this.listBox2_hard.ItemHeight = 16;
+            this.listBox2_hard.Location = new System.Drawing.Point(554, 79);
+            this.listBox2_hard.Name = "listBox2_hard";
+            this.listBox2_hard.Size = new System.Drawing.Size(234, 244);
+            this.listBox2_hard.TabIndex = 6;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 376);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox2_hard);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1_normal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1_easy);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1_easy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1_normal;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox2_hard;
+    }
+}
---
+++ Minesweeper_WindowsFormsApp/Form2.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minesweeper_WindowsFormsApp
+{
+    public partial class Form2 : Form
+    {
+        BindingSource bsEasy = new BindingSource();
+        BindingSource bsNormal = new BindingSource();
+        BindingSource bsHard = new BindingSource();
+
+        public Form2()
+        {
+            InitializeComponent();
+            //asings bindingsource to the results lists
+            bsEasy.DataSource = ResultList.EasyLevel;
+            bsNormal.DataSource = ResultList.NormalLevel;
+            bsHard.DataSource = ResultList.HardLevel;
+
+            //Reads Best scores file , easy level
+            ResultList.EasyLevel.Clear();
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string filePathEasy = currentDirectory;
+            filePathEasy += @"\BestScores_easy.txt";
+            
+            List<string> loadFileEasy = new List<string>();
+            loadFileEasy = File.ReadAllLines(filePathEasy).ToList();
+            foreach(string s in loadFileEasy)
+            {
+                string[] items = s.Split(',');
+                Result one = new Result(items[0], int.Parse(items[1]));
+                ResultList.EasyLevel.Add(one);
+            }
+
+
+            //Reads Best scores file, normal level
+            ResultList.NormalLevel.Clear();
+            string filePathNormal = currentDirectory;
+            filePathNormal += @"\BestScores_normal.txt";
+
+            List<string> loadFileNormal = new List<string>();
+            loadFileNormal = File.ReadAllLines(filePathNormal).ToList();
+            foreach (string s in loadFileNormal)
+            {
+                string[] items = s.Split(',');
+                Result one = new Result(items[0], int.Parse(items[1]));
+                ResultList.NormalLevel.Add(one);
+            }
+
+
+            //Reads Best scores file, hard level
+            ResultList.HardLevel.Clear();
+            string filePathHard = currentDirectory;
+            filePathHard += @"\BestScores_hard.txt";
+
+            List<string> loadFileHard = new List<string>();
+            loadFileHard = File.ReadAllLines(filePathHard).ToList();
+            foreach (string s in loadFileHard)
+            {
+                string[] items = s.Split(',');
+                Result one = new Result(items[0], int.Parse(items[1]));
+                ResultList.HardLevel.Add(one);
+            }
+
+            ResultList.EasyLevel.Sort();
+            ResultList.NormalLevel.Sort();
+            ResultList.HardLevel.Sort();
+
+            //asigns data source for listboxes
+            listBox1_easy.DataSource = bsEasy;
+            listBox1_normal.DataSource = bsNormal;
+            listBox2_hard.DataSource = bsHard;
+        }
+    }
+}
